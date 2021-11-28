8d2d6578e6fa4b10fd9ccdefe34c2bb75be04cca
WNIA <wnia22@gmail.com>
2021-11-28 21:47:05 +0600

    widget_test import update

--- Flutter/flutter_study/test/widget_test.dart
+++ Flutter/flutter_study/test/widget_test.dart
@@ -6,9 +6,9 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
+import 'package:flutter_study/app.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_study/generated_init_app/my_home_page.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
